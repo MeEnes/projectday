0884ca6f5e89729e8ae65c8ab05f52b5d1710dcf
MeEnes <eneskorkmaz@techtorialacademy.com>
2019-07-15 20:51:41 -0500

    firstone

---
+++ src/jenkins/jenkinsUpload.java
@@ -0,0 +1,20 @@
+package jenkins;
+
+public class jenkinsUpload {
+
+
+    public static void main(String[] args) {
+
+
+        for(int i=10; i>0; i--)
+
+            System.out.println("jenkins test ");
+
+
+
+    }
+
+
+
+
+}
